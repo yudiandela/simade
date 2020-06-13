3e7d44764f9b9ff7ab7241ca812e702e4c7269d9
Yudi Andela <yudhi.andhela@gmail.com>
2020-06-14 01:10:16 +0700

    Membuat seeder User

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,51 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Administrator',
+            'email' => 'admin@email.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+            'role' => 'admin'
+        ]);
+
+        User::create([
+            'name' => 'Verificator',
+            'email' => 'verificator@email.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+            'role' => 'verificator'
+        ]);
+
+        User::create([
+            'name' => 'Approval',
+            'email' => 'approval@email.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+            'role' => 'approval'
+        ]);
+
+        User::create([
+            'name' => 'Deployment',
+            'email' => 'deployment@email.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+            'role' => 'deployment'
+        ]);
+    }
+}
